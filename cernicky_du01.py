de951e5b4291e6317ed37e8ab72821d92fe397c2
cernicky.dav <cernicky.dav@gmail.com>
2019-11-06 17:38:14 +0100

    Už funguje všechno!! Zbývá upravit zaokrouhlování a abs u mercatora

--- cernicky_du01.py
+++ cernicky_du01.py
@@ -6,14 +6,14 @@ u = 0
 Rovnobezky = []
 Poledniky = []
 
-#   Uživatelské vstupy
-meritko = input("Jaké chceš měřítko? Zadej celočíselné měřítko: ")
+#   Uživatelské vstupy, ošetřeno o prázdné hodnoty
+meritko = input("Jaké chceš měřítko? Zadej přirozené číslo měřítka: ")
 while meritko == "":
-    meritko = input("Tak znovu... Jaké chceš měřítko? Zadej celočíselné měřítko: ")
+    meritko = input("Tak znovu... Jaké chceš měřítko? Zadej přirozené číslo měřítka: ")
 meritko = int(meritko)
-skok = input("Po kolika stupních chceš měřit? Zadej celočíselné číslo: ")
+skok = input("Po kolika stupních chceš měřit? Zadej přirozené číslo: ")
 while skok == "":
-    skok = input("Tak znovu... Po kolika stupních chceš měřit? Zadej celočíselné číslo: ")
+    skok = input("Tak znovu... Po kolika stupních chceš měřit? Zadej přirozené číslo: ")
 skok = int(skok)
 R = input("Zadej poloměr země v km: ")
 while R == "":
@@ -23,7 +23,6 @@ R = R * 1000000
 if R == 0:
     R = 6371110000
 z = ""
-print(R,skok,meritko)
 #   Funkce pro výpočet k jednotlivým zobrazením
 def Lam(R):
     for v in range(-180, 181, skok):
@@ -67,13 +66,14 @@ def Bra(R):
 def Mer(R):
     for v in range(-180, 181, skok):
         x = R * (v * pi / 180)
-        xm = x / meritko
+        xm = round(x / meritko,2)
         if xm > 1000 or xm < -1000:
             xm = "-"
         Poledniky.append(xm)
     for u in range(-90, 91, skok):
-        y = R * log(tan(2/((90-u)*pi/180)))
-        ym = y / meritko
+        u = abs(u * pi/180)
+        y = R * log(tan((u/2) + pi/4))
+        ym = round(y / meritko,1)
         if ym > 1000 or ym < -1000:
             ym = "-"
         Rovnobezky.append(ym)
@@ -83,14 +83,18 @@ while z != "L" and z != "A" and z != "B" and z != "M":
     z = input("Zadej zkratku jednoho ze zobrazení:\nL - Lambertovo zobrazení \nA - Marinovo zobrazení\nB - Braunovo zobrazení\nM - Mercatorovo zobrazení\nKteré?: ")
     if z == "L":
         Lam(R)
+        print("Lambertovo zobrazení")
     elif z == "A":
         Mar(R)
+        print("Marinovo zobrazení")
     elif z == "B":
         Bra(R)
+        print("Braunovo zobrazení")
     elif z == "M":
         Mer(R)
+        print("Mercatorovo zobrazení")
     else:
         z = input("Neplatný vstup! Pouze:\nL - Lambertovo zobrazení \nA - Marinovo zobrazení\nB - Braunovo zobrazení\nM - Mercatorovo zobrazení\nKteré?: ")
-
+print(f"Měřítko je 1:{meritko}\nMěříš po {skok}°\nPoloměr země je {R} mm")
 print(f"Rovnoběžky (mm): {Rovnobezky}")
 print(f"Poledníky (mm): {Poledniky}")
