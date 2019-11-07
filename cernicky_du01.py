62a73643e71982644ca0c22fcbf517a12dc2d438
cernicky.dav <cernicky.dav@gmail.com>
2019-11-07 10:21:24 +0100

    Hotovson!

--- cernicky_du01.py
+++ cernicky_du01.py
@@ -3,10 +3,12 @@ from math import sqrt,sin,cos,tan,pi,log
 R = 637111000
 v = 0
 u = 0
+z = ""
 Rovnobezky = []
 Poledniky = []
 
 #   Uživatelské vstupy, ošetřeno o prázdné hodnoty
+Z = input("Zadej zkratku jednoho ze zobrazení:\nL - Lambertovo zobrazení \nA - Marinovo zobrazení\nB - Braunovo zobrazení\nM - Mercatorovo zobrazení\nKteré?: ")
 meritko = input("Jaké chceš měřítko? Zadej přirozené číslo měřítka: ")
 while meritko == "":
     meritko = input("Tak znovu... Jaké chceš měřítko? Zadej přirozené číslo měřítka: ")
@@ -22,7 +24,6 @@ R = float(R)
 R = R * 100000
 if R == 0:
     R = 637111000
-z = ""
 #   Funkce pro výpočet k jednotlivým zobrazením
 def Lam(R):
     for v in range(-180, 181, skok):
@@ -80,7 +81,7 @@ def Mer(R):
 
 #   Cyklus vypisování hodnot
 while z != "L" and z != "A" and z != "B" and z != "M":
-    z = input("Zadej zkratku jednoho ze zobrazení:\nL - Lambertovo zobrazení \nA - Marinovo zobrazení\nB - Braunovo zobrazení\nM - Mercatorovo zobrazení\nKteré?: ")
+    z = Z
     if z == "L":
         Lam(R)
         print("Lambertovo zobrazení")
@@ -96,5 +97,41 @@ while z != "L" and z != "A" and z != "B" and z != "M":
     else:
         z = input("Neplatný vstup! Pouze:\nL - Lambertovo zobrazení \nA - Marinovo zobrazení\nB - Braunovo zobrazení\nM - Mercatorovo zobrazení\nKteré?: ")
 print(f"Měřítko je 1:{meritko}\nMěříš po {skok}°\nPoloměr země je {R} mm")
-print(f"Rovnoběžky (mm): {Rovnobezky}")
-print(f"Poledníky (mm): {Poledniky}")
+print(f"Rovnoběžky (cm): {Rovnobezky}")
+print(f"Poledníky (cm): {Poledniky}")
+u = ""
+v = ""
+while u != 0 or v !=0:
+    u = float(input("Zadej zeměpisnou šířku: "))
+    while u < -90 or u > 90:
+        u = float(input("Zadej zeměpisnou šířku v intervalu (-90,90): "))
+    v = float(input("Zadej zeměpisnou délku: "))
+    while v < -180 or u > 180:
+        v = float(input("Zadej zeměpisnou délku v intervalu (-180,180): "))
+
+    if z == "L":
+        x = R * (v * pi / 180)
+        xm = round(x / meritko, 1)
+        y = R * sin(u * pi / 180)
+        ym = round(y / meritko, 1)
+    elif z == "A":
+        x = R * (v * pi / 180)
+        xm = round(x / meritko, 1)
+        y = R * (u * pi / 180)
+        ym = round(y / meritko, 1)
+    elif z == "B":
+        x = R * (v * pi / 180)
+        xm = round(x / meritko, 1)
+        y = 2 * R * tan((u*pi/180)/2)
+        ym = round(y / meritko, 1)
+    elif z == "M":
+        x = R * (v * pi / 180)
+        xm = round(x / meritko, 1)
+        y = R * log(tan((u/2) + pi/4))
+        ym = round(y / meritko, 1)
+    if u == 0 and v == 0:
+        print("Konec!")
+    else:
+        print(f"Zeměpisná šířka je na mapě od středu vzdálena {ym} cm")
+        print(f"Zeměpisná délka je na mapě od středu vzdálena {xm} cm")
+        print("Pokud už nechceš pokračovat zadej 'u' i 'v' rovno 0")
